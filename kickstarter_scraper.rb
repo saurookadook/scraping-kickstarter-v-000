bc7c80ab30de3aa15821ecc3fc7cd3b77894eb01
Andy Maskiell <maskiella@gmail.com>
2018-01-08 22:13:54 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,6 +11,8 @@ def create_project_hash
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {}
   end
+
+  projects
 end
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
